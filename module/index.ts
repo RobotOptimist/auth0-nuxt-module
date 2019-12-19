7387768b4614707ecbecbefe97810cd55b65fc9e
James MacIvor <james.macivor1@gmail.com>
2019-12-19 07:14:29 -0500

    added index.ts with stubbed module

---
+++ module/index.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nuxt/types'
+
+interface Auth0ModuleOptions {
+    
+}
+
+const auth0Module: Module<Auth0ModuleOptions> = function (moduleOptions) {
+
+}
+
+export default auth0Module
